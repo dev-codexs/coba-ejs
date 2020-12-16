e04bd519322477ab7faf1dda023494300be2dc02
robertsuliwa <robertsuliwa@gmail.com>
2020-12-17 06:07:52 +0700

    try folder public

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extend:true}));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -16,7 +16,7 @@ app.use(myLogger)
 
 app.get('/', function (req, res) {
     var name = 'EJS';
-    res.render(__dirname + "/main.html", {name:name, node: 'Node JS'});
+    res.render(__dirname + "/public/main.html", {name:name, node: 'Node JS'});
 })
 app.get('/main', function(req, res) {
   var name = 'hello';
